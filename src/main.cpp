26748c7781c6cb5ce249e8900b9fdfc81207448d
Gerry on MSI <gsihaj5@gmail.com>
2024-12-30 08:54:26 +0700

    lint

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,23 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+  auto window =
+      sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-
-        sf::CircleShape shape(50.f);
-        shape.setFillColor(sf::Color(100, 250, 50));
-        window.draw(shape);
-
-        window.display();
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
+
+    window.clear();
+
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color(100, 250, 50));
+    window.draw(shape);
+
+    window.display();
+  }
 }
